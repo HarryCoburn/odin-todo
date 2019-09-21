3e83f6f8172b456d38599cdece5cec31442a5158
mattibun <mattibun@gmail.com>
2019-09-20 22:06:54 -0400

    More project refinement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import task from './project.js';
+import projectView from './projectView.js';
 
-const defaultProject = task.createNewProject('default');
-
-console.log(task.addTaskToProject(1, 2, 3, 4, defaultProject));
+const defaultProject = task.createNewProject('Default');
+const testProject = task.createNewProject('Test');
 
 const projectList = [];
 projectList[0] = defaultProject;
+projectList[1] = testProject;
 
-console.log(projectList);
+const header = document.createElement('div');
+header.id = 'header';
+document.querySelector('div#content').appendChild(header);
+
+const projectDropLabel = document.createElement('label');
+projectDropLabel.id = 'currProject';
+projectDropLabel.textContent = 'Select Project';
+projectDropLabel.setAttribute('data-currProject', '');
+header.appendChild(projectDropLabel);
+
+projectDropLabel.addEventListener('click', function() {
+  projectView.createDropdown(projectList);
+});
---
+++ src/projectView.js
@@ -0,0 +1,36 @@
+const projectView = (() => {
+  const setProject = project => {
+    document
+      .querySelector('label#currProject')
+      .setAttribute('data-currProject', project);
+    document.querySelector('label#currProject').textContent = project;
+    document.querySelector('.projectDrop').remove();
+  };
+
+  const displayDropdown = dropdown => {
+    const header = document.querySelector('div#header');
+    header.appendChild(dropdown);
+  };
+
+  const createDropdown = projects => {
+    if (document.querySelector('.projectDrop')) return;
+    const projectNames = projects.map(project => project.name);
+    const projectDropList = document.createElement('ul');
+    projectDropList.classList.add('nobull', 'projectDrop');
+    projectNames.forEach(item => {
+      const currItem = document.createElement('li');
+      currItem.textContent = item;
+      currItem.addEventListener('click', function() {
+        setProject(item);
+      });
+      projectDropList.appendChild(currItem);
+    });
+    displayDropdown(projectDropList);
+  };
+
+  return {
+    createDropdown,
+  };
+})();
+
+export default projectView;
